9fc237a31c5feeac276da92c64e30ddce89acf3a
sille-zoska <sille@zochova.sk>
2025-04-25 10:29:54 +0200

    strings added

---
+++ strings.py
@@ -0,0 +1,14 @@
+predstavenie_sa = "Volam sa Gabriel Sille"
+meno = "Gabriel"
+priezvisko = "Sille"
+
+
+pocet_znakov = len(predstavenie_sa)
+cele_meno = meno + " " + priezvisko
+
+
+print("Pocet znakov v mojom mene je: ", pocet_znakov)
+print("Prvy znak je: ", predstavenie_sa[0])
+print("Cele meno je: ", cele_meno)
+print("Ahoj, " + meno + " " + priezvisko)
+print(len(predstavenie_sa))
