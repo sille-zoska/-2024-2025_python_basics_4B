81620534d50dd3c757db8549d0a3470668368bf1
sille-zoska <sille@zochova.sk>
2025-05-14 13:28:23 +0200

    loops added

--- list.py
+++ list.py
@@ -3,20 +3,24 @@ cisla = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 znaky = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
 matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-print("Pismena: ", pismena)
+print( pismena)
 print("Cisla: ", cisla)
 print("Znaky: ", znaky) 
 print("Matrix: ", matrix)
+
+print("Sucet:", pismena + cisla)
+
+print("Pocet :", len(pismena))
+
 print("Pismena[2]: ", pismena[2])
-print("Cisla[4]: ", cisla[4])
-print("Znaky[6]: ", znaky[6])
-print("Matrix[1][2]: ", matrix[1][2])
-print("Pismena[2]: ", pismena[:2])
-print("Cisla[4]: ", cisla[:4])
-print("Znaky[6]: ", znaky[:6])
-print("Matrix[1][2]: ", matrix[1][:2])
-print("Pismena[2]: ", pismena[2:])
-print("Cisla[4]: ", cisla[4:])
-print("Znaky[6]: ", znaky[6:])
-print("Matrix[1][2]: ", matrix[1][2:])
-print("Sucet", pismena + cisla)
+# print("Cisla[4]: ", cisla[4])
+# print("Znaky[6]: ", znaky[6])
+# print("Matrix[1][2]: ", matrix[1][2])
+# print("Pismena[2]: ", pismena[:2])
+# print("Cisla[4]: ", cisla[:4])
+# print("Znaky[6]: ", znaky[:6])
+# print("Matrix[1][2]: ", matrix[1][:2])
+# print("Pismena[2]: ", pismena[2:])
+# print("Cisla[4]: ", cisla[4:])
+# print("Znaky[6]: ", znaky[6:])
+# print("Matrix[1][2]: ", matrix[1][2:])
---
+++ loops.py
@@ -0,0 +1,22 @@
+
+pismena = ["a", "b", "c", "d", "f", "g", "h", "i", "j"]
+
+# for i in range (len(pismena)):    
+#     print("Hello world")
+
+
+pocet = 1
+
+while pocet <= 10:
+    print("Hello world")
+    print("Pocet pred inkrementaciou je: ", pocet)
+    pocet += 1
+    print("Pocet po inkrementacii je: ", pocet)
+
+
+for i in range(pocet, 11):
+    print("Hello world")
+    print("i je: ", i)
+
+
+print("Koniec")
