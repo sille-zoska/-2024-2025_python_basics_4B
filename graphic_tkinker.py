f8f4a547bc80a39cdbc670c4ad55da273e8ebf53
sille-zoska <sille@zochova.sk>
2025-05-16 10:09:08 +0200

    all added

---
+++ graphic_tkinker.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+okno = tk.Tk()
+okno.title("Moje okno")
+
+stitek = tk.Label(okno, text="Ahoj svet!")
+stitek.pack()
+
+okno.mainloop()
--- randomize.py
+++ randomize.py
@@ -5,4 +5,5 @@ print("Náhodné číslo medzi 1 a 10:", cislo)
 cislo = random.randint(1, 100)
 print("Náhodné číslo medzi 1 a 100:", cislo)
 cislo = random.randint(1, 1000)
-print("Náhodné číslo medzi 1 a 1000:", cislo)
+print("Náhodné číslo medzi 1 a 1000:", cislo) 
+
